394184e6b4e56be2d3ca5ce0fee832b5c5db533c
Rxbsxn <harezlak.robert7@gmail.com>
2016-03-24 18:18:17 +0100

    added kudo counter

--- app/controllers/people.js
+++ app/controllers/people.js
@@ -1,10 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  kudo: 0,
+
+
   actions: {
    giveKudo(person) {
-     person.deleteRecord();
+     this.incrementProperty('kudo');
      person.save();
    },
+   delete(person) {
+    person.deleteRecord();
+    person.save();
+    },
  },
 });
